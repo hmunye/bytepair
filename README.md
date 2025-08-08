df8323f856d3a921096eb8d85bbe05c691487c0a
Huseen <136820327+hmunye@users.noreply.github.com>
2025-08-08 19:43:18 -0400

    start: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# bytepair
+
+`Byte-Pair Encoding` (BPE) implementation from scratch, designed specifically for tokenization tasks 
+in NLP and text processing.
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! [Byte-Pair Encoding] (BPE) implementation from scratch, designed
+//! specifically for tokenization tasks in NLP and text processing.
+//!
+//! [Byte-Pair Encoding]: https://en.wikipedia.org/wiki/Byte-pair_encoding
+
+#![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
